74683ea48fb7ae106f084542be8a718e15e7661b
Matt Masson <mmasson@microsoft.com>
2021-12-02 16:27:59 -0500

    add basic config support

---
+++ src/Configuration.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface Configuration {
+    PQTestLocation: string;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { Configuration } from "./Configuration";
+
+let currentConfigruation: Configuration;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,13 +18,24 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand("powerquery-sdk.helloWorld", () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World from powerquery-sdk!");
-    });
+    context.subscriptions.push(
+        vscode.commands.registerCommand("powerquery-sdk.helloWorld", () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("Hello World from powerquery-sdk!");
+        }),
+    );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeConfiguration(event => {
+            if (event.affectsConfiguration("powerquery.sdk")) {
+                const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("powerquery.sdk");
+                currentConfigruation = {
+                    PQTestLocation: config?.get("pqtest.location") as string,
+                };
+            }
+        }),
+    );
 }
 
 // this method is called when your extension is deactivated
