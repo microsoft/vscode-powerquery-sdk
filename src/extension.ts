ebe6a79d287f413c94a06a189e98fa028d237e25
Matt Masson <matt.masson@microsoft.com>
2021-12-14 08:15:38 -0500

    Add PQTest TaskProvider (#2)

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,38 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { ExtensionSettings } from "./ExtensionSettings";
+import { PowerQueryTaskProvider } from "./PowerQueryTaskProvider";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "powerquery-sdk" is now active!');
+let pqTaskProvider: vscode.Disposable | undefined;
+//let currentSettings: ExtensionSettings | undefined;
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand("powerquery-sdk.helloWorld", () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World from powerquery-sdk!");
-    });
+export function activate(_context: vscode.ExtensionContext) {
+    pqTaskProvider = vscode.tasks.registerTaskProvider(
+        PowerQueryTaskProvider.TaskType,
+        new PowerQueryTaskProvider(fetchExtensionSettings),
+    );
 
-    context.subscriptions.push(disposable);
+    // Listen for configuration changes
+    // context.subscriptions.push(
+    //     vscode.workspace.onDidChangeConfiguration(event => {
+    //         if (event.affectsConfiguration("powerquery.sdk")) {
+    //             currentSettings = fetchExtensionSettings();
+    //         }
+    //     }),
+    // );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {
+    if (pqTaskProvider) {
+        pqTaskProvider.dispose();
+    }
+}
+
+function fetchExtensionSettings(): ExtensionSettings {
+    const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("powerquery.sdk");
+    return {
+        PQTestLocation: config?.get("pqtest.location") as string,
+    };
+}
