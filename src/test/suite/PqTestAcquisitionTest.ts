a0f6be9f14aecaa16bf069069ea045770bc704e2
Albert Li <wentaoli@microsoft.com>
2022-09-02 15:29:45 +0800

    feat. consume public nuget p2: enable test cases

--- src/test/suite/PqTestAcquisitionTest.ts
+++ src/test/suite/PqTestAcquisitionTest.ts
@@ -8,61 +8,20 @@
 import * as chai from "chai";
 import * as fs from "fs";
 import * as path from "path";
-import * as process from "process";
 import * as vscode from "vscode";
 
 import { ExtensionConstants } from "../../constants/PowerQuerySdkExtension";
+import { NugetHttpService } from "../../common/NugetHttpService";
 
 import { NugetVersions } from "../../utils/NugetVersions";
-import { SpawnedProcess } from "../../common/SpawnedProcess";
 
-import {
-    buildPqTestSubPath,
-    extensionDevelopmentPath,
-    NugetBaseFolder,
-    NugetConfigFileName,
-    PqTestSubPath,
-} from "./common";
+import { buildPqTestSubPath, extensionDevelopmentPath, NugetBaseFolder, PqTestSubPath } from "./common";
 import { delay } from "./utils";
 
 const expect = chai.expect;
 
 const MAX_AWAIT_TIME: number = 2 * 60e3;
 const AWAIT_INTERVAL: number = 5e3;
-
-async function doListPqTestFromNuget(): Promise<string> {
-    // nuget list Microsoft.PowerQuery.SdkTools -ConfigFile ./etc/nuget-staging.config
-    const baseNugetFolder: string = path.resolve(extensionDevelopmentPath, NugetBaseFolder);
-
-    if (!fs.existsSync(baseNugetFolder)) {
-        fs.mkdirSync(baseNugetFolder);
-    }
-
-    const args: string[] = [
-        "list",
-        ExtensionConstants.InternalMsftPqSdkToolsNugetName,
-        "-ConfigFile",
-        path.resolve(extensionDevelopmentPath, "etc", NugetConfigFileName),
-    ];
-
-    const seizingProcess: SpawnedProcess = new SpawnedProcess("nuget", args, {
-        cwd: baseNugetFolder,
-        env: {
-            ...process.env,
-            FORCE_NUGET_EXE_INTERACTIVE: "true",
-        },
-    });
-
-    await seizingProcess.deferred$;
-
-    return seizingProcess.stdOut;
-}
-
-async function getLatestNugetVersion(): Promise<string | undefined> {
-    const latestVersion: NugetVersions = NugetVersions.createFromNugetListOutput(await doListPqTestFromNuget());
-
-    return latestVersion.toString();
-}
 
 function getExpectedPqTestPath(maybeNextVersion?: string): string | undefined {
     const baseNugetFolder: string = path.resolve(extensionDevelopmentPath, NugetBaseFolder);
@@ -73,10 +32,23 @@ function getExpectedPqTestPath(maybeNextVersion?: string): string | undefined {
 }
 
 export function buildPqTestAcquisitionTest(): void {
-    test("Seize the pqTest from nuget", async () => {
+    const nugetHttpService = new NugetHttpService();
+
+    test("Seize the pqTest from public nuget", async () => {
+        const packageName: string = ExtensionConstants.PublicMsftPqSdkToolsNugetName;
+        const releasedVersions = await nugetHttpService.getPackageReleasedVersions(packageName);
+
+        const nugetVersions: NugetVersions[] = releasedVersions.versions.map((releasedVersion: string) =>
+            NugetVersions.createFromReleasedVersionString(releasedVersion),
+        );
+
+        expect(nugetVersions.length).gt(0);
+
         let i = 0;
 
-        const mayBeExpectedPqTestExePath: string | undefined = getExpectedPqTestPath(await getLatestNugetVersion());
+        const mayBeExpectedPqTestExePath: string | undefined = getExpectedPqTestPath(
+            nugetVersions[nugetVersions.length - 1].toString(),
+        );
 
         expect(typeof mayBeExpectedPqTestExePath).eq("string");
 
