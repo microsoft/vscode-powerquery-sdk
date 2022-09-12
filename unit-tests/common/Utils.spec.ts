b3f0b7a15d45c1aa5858954aa5b9e86ab6f3c1fe
Albert Li <wentaoli@microsoft.com>
2022-09-12 19:57:25 +0800

    chore. rm unneeded unit test cases

---
+++ unit-tests/common/Utils.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the MIT license found in the
+ * LICENSE file in the root of this projects source tree.
+ */
+
+import * as chai from "chai";
+import * as fs from "fs";
+
+import { makeOneTmpDir } from "../../src/utils/osUtils";
+import { removeDirectoryRecursively } from "../../src/utils/files";
+
+const expect = chai.expect;
+
+describe("Utils unit testes", () => {
+    it("Create a tmp directory", async () => {
+        const oneTmpDir = makeOneTmpDir();
+
+        expect(fs.existsSync(oneTmpDir)).true;
+
+        await removeDirectoryRecursively(oneTmpDir);
+    });
+});
