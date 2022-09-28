b4f1eb59e727ad5d8cefd7c08d8355c98ad02b82
Albert Li <wentaoli@microsoft.com>
2022-09-28 14:22:13 +0800

    fix. e2e test again the expected drop output p3

--- scripts/test-e2e.ts
+++ scripts/test-e2e.ts
@@ -4,6 +4,7 @@
  * Licensed under the MIT license found in the
  * LICENSE file in the root of this projects source tree.
  */
+import * as path from "path";
 
 import { ExTester } from "vscode-extension-tester";
 import { getFirstVsixFileDirectlyBeneathOneDirectory } from "./utils/vsixs";
@@ -13,8 +14,13 @@ const theVsixFilePath: string = getFirstVsixFileDirectlyBeneathOneDirectory(proc
 async function doE2eTest() {
     const extTest = new ExTester();
 
-    await extTest.installVsix({ vsixFile: theVsixFilePath });
+    // Performs all necessary setup: getting VSCode + ChromeDriver and packaging/installing extension into the test instance
+    await extTest.setupRequirements();
 
+    // Install the extension into the test instance of VS Code
+    await extTest.installVsix({ vsixFile: path.resolve(process.cwd(), theVsixFilePath) });
+
+    // Runs the selected test files in VS Code using mocha and webdriver
     await extTest.runTests("out/src/test/**/*.spec.js", { cleanup: true });
 }
 
