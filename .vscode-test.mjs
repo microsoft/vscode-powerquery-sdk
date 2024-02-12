c896b4b72e4551ec7bd2f8924f4026673bd0ceba
Matt Masson <mmasson@microsoft.com>
2024-02-12 13:18:43 -0500

    working with empty test

---
+++ .vscode-test.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from '@vscode/test-cli';
+
+export default defineConfig({
+	files: 'out/src/test/**/*.test.js',
+});
---
+++ src/test/suite/index.ts
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the MIT license found in the
+ * LICENSE file in the root of this projects source tree.
+ */
+
+import * as path from "path";
+import glob from "glob";
+import Mocha from "mocha";
+
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: "tdd",
+        color: true,
+    });
+
+    const testsRoot = path.resolve(__dirname, "..");
+
+    return new Promise((c, e) => {
+        glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
+            }
+        });
+    });
+}
